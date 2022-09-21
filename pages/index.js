ec762227578e640e859e1845d06c643396fa6982
mateonunez <mateonunez95@gmail.com>
2022-09-21 09:42:22 +0200

    perf(*): improve ux ui and small things

--- pages/index.js
+++ pages/index.js
@@ -73,122 +73,82 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mt-10">
+      <main className="min-h-screen mx-auto mt-10">
         <h1 className="text-xl font-bold lg:text-4xl">
           Lyra Impact supercharged 🌍☄️️
         </h1>
 
-        {/* Form */}
-        <form
-          className="container pt-10"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(fetcher, property, endpoint);
-          }}
-        >
-          {/* Fetcher */}
-          <div className="p-3">
-            <label>Fetcher</label>
-            <div className="relative">
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5"
-                  />
-                </svg>
-              </div>
-
-              <select
-                value={fetcher}
-                onChange={(e) => setFetcher(e.target.value)}
-                className="pl-10"
+        {/* Fetcher */}
+        <div className="p-3 mt-10">
+          <label>Fetcher</label>
+          <div className="relative">
+            <div className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                {Object.entries(fetchers).map(([key, value]) => (
-                  <option key={key} value={value}>
-                    {key.toUpperCase()}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-
-          {/* Endpoint */}
-          <div className="p-3">
-            <label>Endpoint</label>
-            <div className="relative">
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                  />
-                </svg>
-              </div>
-
-              <input
-                className="pl-10"
-                type="text"
-                placeholder="Endpoint"
-                value={endpoint}
-                onChange={(e) => setEndpoint(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-
-          {/* GraphQL query */}
-          {fetcher === "graphql" && (
-            <div className="p-3">
-              <label>GraphQL query</label>
-              <div className="relative">
-                <div className="icon">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
-                    />
-                  </svg>
-                </div>
-
-                <textarea
-                  placeholder="GraphQL query"
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                  className="pl-10"
-                  rows="5"
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5"
                 />
-              </div>
+              </svg>
             </div>
-          )}
 
-          {/* Property */}
+            <select
+              value={fetcher}
+              onChange={(e) => setFetcher(e.target.value)}
+              className="pl-10"
+            >
+              {Object.entries(fetchers).map(([key, value]) => (
+                <option key={key} value={value}>
+                  {key.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* Endpoint */}
+        <div className="p-3">
+          <label>Endpoint</label>
+          <div className="relative">
+            <div className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </div>
+
+            <input
+              className="pl-10"
+              type="text"
+              placeholder="Endpoint"
+              value={endpoint}
+              onChange={(e) => setEndpoint(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        {/* GraphQL query */}
+        {fetcher === "graphql" && (
           <div className="p-3">
-            <label>Property</label>
+            <label>GraphQL query</label>
             <div className="relative">
               <div className="icon">
                 <svg
@@ -202,30 +162,52 @@ export default function Home() {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                    d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
                   />
                 </svg>
               </div>
 
-              <input
+              <textarea
+                placeholder="GraphQL query"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
                 className="pl-10"
-                type="text"
-                placeholder="Property"
-                value={property}
-                onChange={(e) => setProperty(e.target.value)}
+                rows="5"
               />
             </div>
           </div>
+        )}
 
-          <div className="p-3">
-            <button
-              type="submit"
-              className="text-white bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              ☄️️
-            </button>
+        {/* Property */}
+        <div className="p-3">
+          <label>Property</label>
+          <div className="relative">
+            <div className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                />
+              </svg>
+            </div>
+
+            <input
+              className="pl-10"
+              type="text"
+              placeholder="Property"
+              value={property}
+              onChange={(e) => setProperty(e.target.value)}
+            />
           </div>
-        </form>
+        </div>
 
         {/* Schema */}
         {schema && (
@@ -334,7 +316,7 @@ export default function Home() {
             {results?.hits?.length > 0 &&
               results?.hits.map((hit) => (
                 <div
-                  className="flex flex-col p-3 my-3 overflow-hidden rounded shadow-lg full-width"
+                  className="flex flex-col p-3 my-5 overflow-hidden rounded shadow-lg full-width"
                   key={hit.id}
                 >
                   {Object.keys(hit).map((_key) => (
@@ -368,7 +350,8 @@ export default function Home() {
         )}
       </main>
 
-      <footer className="fixed bottom-0 pt-10">
+      {/* Footer  */}
+      <footer className="bottom-0 py-5">
         Created by{" "}
         <a
           href="https://github.com/mateonunez"
