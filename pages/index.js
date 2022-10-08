5ac2ccae32499d44dd7a56f3be2e8e36d2a4ff1e
mateonunez <mateonunez95@gmail.com>
2022-10-08 14:38:07 +0200

    feat(*): simple implementation of components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
+import { impact } from "@mateonunez/lyra-impact";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { CrossIcon, MagnifyingGlassIcon } from "../components/icons";
 import ContainerImage from "../components/ui/containers/container-image";
+import EndpointInput from "../components/ui/inputs/endpoint-input";
+import Input from "../components/ui/inputs/input";
 import config from "../lib/config/config.json";
 
 export default function Homepage() {
+  const [lyra, setLyra] = useState(null);
+
+  const endpointCallback = useCallback((endpoint, config) => {
+    impact(endpoint, config).then((lyra) => setLyra(lyra));
+  }, []);
+
   return (
-    <ContainerImage src="/lyra-supercharged-dalle.tiny.png">
-      <div className="mt-5 text-center">
-        <h1 className="title">{config.appName}</h1>
+    <>
+      <div className="relative z-50">
+        <div className="flex mt-5 text-center">
+          <h1 className="title">{config.appName}</h1>
+        </div>
+
+        <div className="my-20 text-center">
+          <h2 className="text-2xl">Quotes or something chill....</h2>
+        </div>
       </div>
-    </ContainerImage>
+
+      <ContainerImage classNameContent="flex justify-between" src="/lyra-supercharged-dalle.tiny.png">
+        <div className="w-full px-10 my-auto md:m-auto md:w-2/3">
+          <EndpointInput callback={endpointCallback} />
+        </div>
+      </ContainerImage>
+    </>
   );
 }
