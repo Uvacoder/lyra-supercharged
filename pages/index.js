c7d26c64a3818d1fb3b61c4db311ab3797a5e4b9
mateonunez <mateonunez95@gmail.com>
2022-10-08 15:49:19 +0200

    chore(*): work in progress

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,30 @@ import { CrossIcon, MagnifyingGlassIcon } from "../components/icons";
 import ContainerImage from "../components/ui/containers/container-image";
 import EndpointInput from "../components/ui/inputs/endpoint-input";
 import Input from "../components/ui/inputs/input";
+import Loading from "../components/ui/loading";
 import config from "../lib/config/config.json";
 
 export default function Homepage() {
+  const [fetchingEndpoint, setFetchingEndpoint] = useState(false);
   const [lyra, setLyra] = useState(null);
 
   const endpointCallback = useCallback((endpoint, config) => {
+    setFetchingEndpoint(true);
+
     impact("/api/forward", {
       fetch: {
         method: "POST",
-        property: "profile.repositories",
         body: JSON.stringify({
           endpoint,
         }),
       },
     })
-      .then((lyra) => {
-        console.log(lyra);
+      .then((lyra) => setLyra(lyra))
+      .catch((error) => {
+        // handle error here, should I provide `property` input?
+        console.error(error);
       })
-      .catch((error) => console.error(error));
+      .finally(() => setFetchingEndpoint(false));
   }, []);
 
   return (
@@ -39,7 +44,8 @@ export default function Homepage() {
 
       <ContainerImage classNameContent="flex justify-between" src="/lyra-supercharged-dalle.tiny.png">
         <div className="w-full px-10 my-auto md:m-auto md:w-2/3">
-          <EndpointInput callback={endpointCallback} />
+          {fetchingEndpoint && <Loading />}
+          {lyra ? <span>Fetched</span> : <EndpointInput callback={endpointCallback} />}
         </div>
       </ContainerImage>
     </>
