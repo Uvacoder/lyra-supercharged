cc9c22ef8dd5249c8aed2992718d55e713c511ce
mateonunez <mateonunez95@gmail.com>
2022-10-08 14:37:28 +0200

    chore(icons): rename with standard

--- components/icons/magnifying-glass.js
+++ components/icons/magnifying-glass.js
@@ -1,4 +1,4 @@
-export default function MagnifyingGlass({ ...props }) {
+export default function MagnifyingGlassIcon({ ...props }) {
   return (
     <>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
