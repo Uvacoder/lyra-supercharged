57074b241b573c53357615a9b652d72521c687f2
mateonunez <mateonunez95@gmail.com>
2022-10-09 16:13:00 +0200

    chore(*): add simple results table

---
+++ components/tables/results-table.js
@@ -0,0 +1,7 @@
+export default function ResultsTable({ results, matches, ...props }) {
+  console.log({
+    results,
+    matches,
+  });
+  return <></>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
+import { search } from "@lyrasearch/lyra";
 import { impact } from "@mateonunez/lyra-impact";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { CrossIcon, MagnifyingGlassIcon } from "../components/icons";
-import ContainerImage from "../components/ui/containers/container-image";
-import EndpointInput from "../components/ui/inputs/endpoint-input";
-import Input from "../components/ui/inputs/input";
-import Loading from "../components/ui/loading";
+import { useCallback, useState } from "react";
+import ContainerImage from "../components/containers/container-image";
+import { EndpointInput, SearchInput } from "../components/inputs/";
 import config from "../lib/config/config.json";
+import cn from "classnames";
+import { match } from "@mateonunez/lyra-match";
+import { ResultsTable } from "../components/tables";
 
 export default function Homepage() {
   const [fetchingEndpoint, setFetchingEndpoint] = useState(false);
   const [lyra, setLyra] = useState(null);
+  const [results, setResults] = useState([]);
+  const [matches, setMatches] = useState([]);
 
   const endpointCallback = useCallback((endpoint, config) => {
     setFetchingEndpoint(true);
@@ -30,24 +33,53 @@ export default function Homepage() {
       .finally(() => setFetchingEndpoint(false));
   }, []);
 
+  const searchCallback = useCallback(
+    (term) => {
+      if (term) {
+        const results = search(lyra, { term });
+        const matches = match(results.hits, { term });
+        setResults(results);
+        setMatches(matches);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [lyra]
+  );
+
   return (
     <>
-      <div className="relative z-50">
-        <div className="flex mt-5 text-center">
+      <div className="relative z-50 flex flex-col justify-between h-full">
+        {/* Title */}
+        <div className="mx-auto mt-5 text-center">
           <h1 className="title">{config.appName}</h1>
         </div>
 
-        <div className="my-20 text-center">
+        {/* <div className="my-20 text-center">
           <h2 className="text-2xl">Quotes or something chill....</h2>
+        </div> */}
+
+        {/* Search input */}
+        {lyra && (
+          <div className="w-full p-10 mx-auto mt-0 md:w-2/3">
+            <SearchInput autofocus callback={searchCallback} />
+          </div>
+        )}
+
+        {/* Enpoint input */}
+        {!lyra && (
+          <div className="w-full px-10 md:m-auto md:w-2/3">
+            <EndpointInput callback={endpointCallback} />
+          </div>
+        )}
+
+        {/* Results */}
+        <div className="w-full px-10 my-auto md:m-auto">
+          <ResultsTable results={results} matches={matches} />
+          {/* {results && results.hits && results.hits.length > 0 &&  />} */}
         </div>
       </div>
 
-      <ContainerImage classNameContent="flex justify-between" src="/lyra-supercharged-dalle.tiny.png">
-        <div className="w-full px-10 my-auto md:m-auto md:w-2/3">
-          {fetchingEndpoint && <Loading />}
-          {lyra ? <span>Fetched</span> : <EndpointInput callback={endpointCallback} />}
-        </div>
-      </ContainerImage>
+      <ContainerImage classNameContent="relative" src="/lyra-supercharged-dalle.tiny.png" />
     </>
   );
 }
